65c905711943cbc698c58ba639b41c7918f005cb
KITAGAWA Daisuke <dogwood008+github@gmail.com>
2025-11-09 21:13:28 +0900

    Restore node prefix for path import

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import path from "path";
+import path from "node:path";
 import { createYearCalendar, parseYear } from "./calendar";
 
 const DEFAULT_PORT = Number.parseInt(process.env.PORT ?? "3000", 10);
