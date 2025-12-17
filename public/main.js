7ccf3224a1daf5c4e20c16e317b1fc9ee12ec276
KITAGAWA Daisuke <dogwood008+github@gmail.com>
2025-12-17 16:49:28 +0900

    Explain client decode duplication
    https://github.com/dogwood008/csv_to_pl_calendar/pull/16#discussion_r2625937570

--- public/main.js
+++ public/main.js
@@ -51,6 +51,7 @@ const DEFAULT_SPREADSHEET_ENDPOINT =
 const DEFAULT_SPREADSHEET_PSK = "testpsk";
 const UTF8_DECODER = new TextDecoder("utf-8");
 
+// サーバー側の src/csv/decodeCsv.ts と同等のエンコーディング判定をブラウザ単体で実行するため、重複実装をここに保持する。
 let shiftJisDecoder = null;
 try {
   shiftJisDecoder = new TextDecoder("shift_jis");
