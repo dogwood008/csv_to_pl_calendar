28cce29f9c32a5b4c87229f521749fc96559bf0b
KITAGAWA Daisuke <dogwood008+github@gmail.com>
2025-12-19 04:53:52 +0000

    デフォルトCSVをビルド成果物へ静的コピー
    https://github.com/dogwood008/csv_to_pl_calendar/pull/17#discussion_r2633553041

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   root: "public",
   base: "./",
   publicDir: false,
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "data/dummy_kabucom.csv",
+          dest: "data",
+        },
+      ],
+    }),
+  ],
   server: {
     fs: {
       allow: [".."],
