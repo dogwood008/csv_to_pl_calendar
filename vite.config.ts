5c1825fa7c310b884b194c4fe676de9ea572ccff
KITAGAWA Daisuke <dogwood008+github@gmail.com>
2025-12-20 03:55:19 +0000

    vite devでCSVの静的コピーを監視
    https://github.com/dogwood008/csv_to_pl_calendar/pull/17#discussion_r2636749800

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,18 @@ export default defineConfig({
   root: "public",
   base: "./",
   publicDir: false,
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: "data/dummy_kabucom.csv",
-          dest: "data",
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: "data/dummy_kabucom.csv",
+            dest: "data",
+          },
+        ],
+        watch: {
+          reloadPageOnChange: true,
         },
-      ],
-    }),
+      }),
   ],
   server: {
     fs: {
